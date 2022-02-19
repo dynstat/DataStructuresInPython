8f9abe0a3c91219570a70745ffb019c0b8800204
dynstat <vivek15@gmx.com>
2022-02-19 18:44:55 +0530

    rough folder removed

--- rough/class.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    # First argument of the method is always considered as the self irrespective of its name.
-    def print_something(self, name):
-        print(f"yo {name}")
-
-
-if __name__ == "__main__":
-    test1 = Test()
-    test1.print_something("XS")
