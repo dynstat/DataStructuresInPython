38335d95c1ba0594a4c82a8379f1d481f9abf29d
kanstat <kanchan.sharma9722@gmail.com>
2022-03-13 21:06:44 +0530

    binary tree file is added.

---
+++ kanchan/b.tree.py
@@ -0,0 +1,37 @@
+# full tree is a tree which has zero child nodes or two child.
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def isfullTree(root):
+    if root is None:
+        return True
+
+    if root.left is None and root.right is None:
+        return True
+
+    if root.left is not None and root.right is not None:
+        return (isfullTree(root.left)) and (isfullTree(root.right))
+
+    return False
+
+
+def Inorder_traversal(root):
+    if root:
+        Inorder_traversal(root.left)
+        print(root.data)
+        Inorder_traversal(root.right)
+
+
+if __name__ == "__main__":
+    root = Node(56)
+    root.left = Node(50)
+    root.left.right = Node(65)
+    root.left.left = Node(60)
+    root.right = Node(55)
+    root.right.left = Node(40)
+
+    print(isfullTree(root))
