1685e42085abd02b466d0ac353ea899417d10ff4
kanstat <kanchan.sharma9722@gmail.com>
2022-03-24 22:42:57 +0530

    Traversal method in tree with class file modified.

--- kanchan/tree_with_class.py
+++ kanchan/tree_with_class.py
@@ -8,14 +8,18 @@ class Node:
 
 class Tree:
     def __init__(self, root):
-        self.root = root
+        self.r = root
+        root = None
 # inorder traversal
+
+    def inorder_wrapper_traversal(self):
+        self.inorder_Traversal(self.r)
 
     def inorder_Traversal(self, root):
         if root:
-            self.inorder_Traversal(root.right)
-            print(self.data)
             self.inorder_Traversal(root.left)
+            print(root.data)
+            self.inorder_Traversal(root.right)
 
     def preorder_Traversal(self, root):
         if root:
@@ -27,3 +31,18 @@ class Tree:
         if root:
             self.postorder_Traversal(root.left)
             self.postorder_Traversal(root.right)
+            print(root.data)
+
+
+if __name__ == "__main__":
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.right.right.left = Node(8)
+    tree1 = Tree(root)
+
+    tree1.inorder_wrapper_traversal()
