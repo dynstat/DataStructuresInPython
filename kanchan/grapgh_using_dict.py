087d2e6bd33a4a4ed7d59525e0c4f2e6b1b718e5
kanstat <kanchan.sharma9722@gmail.com>
2022-04-07 18:46:20 +0530

    three files added.

---
+++ kanchan/grapgh_using_dict.py
@@ -0,0 +1,36 @@
+# creating class of graph
+class graph:
+    def __init__(self, graph_element: dict = {}):
+        self.graph = graph_element
+
+    def show_edges(self):
+        edge_ki_list = []
+        for key, val in self.graph.items():
+            for value in val:
+                if (value, key) not in edge_ki_list:
+                    edge_ki_list.append((key, value))
+        return edge_ki_list
+
+    def add_edges(self, edges: tuple = None):  # (f,g)
+        v1, v2 = edges
+        if v1 not in self.graph:
+            self.graph[v1] = []
+        if v2 not in self.graph:
+            self.graph[v2] = []
+        if v2 not in self.graph[v1]:
+            self.graph[v1].append(v2)
+        if v1 not in self.graph[v2]:
+            self.graph[v2].append(v1)
+
+
+if __name__ == "__main__":
+    graph_ele = {"a": ["b", "c"],
+                 "b": ["a", "d"],
+                 "c": ["a", "d"],
+                 "d": ["e"],
+                 "e": ["d"]}
+
+    g1 = graph(graph_ele)
+    print(g1.show_edges())
+    g1.add_edges(("f", "g"))
+    print(g1.show_edges())
