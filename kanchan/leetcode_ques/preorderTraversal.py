dd26afae157aefd4c7cea21a51516d3c4b1ec9ba
dynstat <vivek15@gmx.com>
2022-04-08 14:22:59 +0530

    comments added

--- kanchan/leetcode_ques/preorderTraversal.py
+++ kanchan/leetcode_ques/preorderTraversal.py
@@ -27,23 +27,26 @@ class Tree:
         self.root = root
         self.empty_list = []
 
-    def preorderTraverval(self, root):
-
-        if root:
-            self.empty_list.append(root.value)
-            self.preorderTraverval(root.left)
-            self.preorderTraverval(root.right)
+    def preorderTraverval(self, r):
+        if r:
+            self.empty_list.append(r.value)
+            self.preorderTraverval(r.left)
+            self.preorderTraverval(r.right)
         return self.empty_list
 
 
 if __name__ == "__main__":
+    # creating nodes for the tree
     node1 = Node(1)
     node2 = Node(2)
     node3 = Node(3)
-
+    ##############################
+    # Creating tree with the root node named as "root"
     root = node1
     node1.right = node2
     node2.left = node3
+    ##############################
 
+    # Creating Tree object and passing our created tree in it.
     tree_obj = Tree(root)
     print(tree_obj.preorderTraverval(root))
