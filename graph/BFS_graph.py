399f052877335ad846cc4516ce1360497369ceed
dynstat <vivek15@gmx.com>
2022-04-09 17:11:22 +0530

    bfs in graph.. Node and graph classes created

--- graph/BFS_graph.py
+++ graph/BFS_graph.py
@@ -1,1 +1,25 @@
-# BFS traversal in the graph
+# BFS traversal in the graph using queue (deque from the collections package)
+
+from collections import deque
+
+
+class Node:
+    def __init__(self, val) -> None:
+        self.value = val
+        self.next = None
+
+
+class Graph:
+    def __init__(self, someGraph: Node) -> None:
+        self.graph = someGraph
+        self.visited = set()  # empty set to store visited nodes
+        # self.parent = {}
+        # self.level = {}
+
+    def bfs(self):
+        q = deque([self.graph])
+        while q:
+            first = q.popleft()
+            if first not in self.visited:
+                print(first)
+            self.visited.add(first)
