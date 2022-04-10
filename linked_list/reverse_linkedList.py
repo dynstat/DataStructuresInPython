ab8bb3a1612ee7d4b385fce53b58c73bafcd2881
Kanchan Sharma <86715068+kanstat@users.noreply.github.com>
2022-04-10 23:25:06 +0530

    created linked list class and Node class

--- linked_list/reverse_linkedList.py
+++ linked_list/reverse_linkedList.py
@@ -1,1 +1,38 @@
+# program to reverse a given linked list
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def traverse(self):
+        if self.head is None:
+            print("Linked List is Empty !!")
+        else:
+            temp = self.head
+            while(temp != None):
+                print(temp.data)
+                temp = temp.next
+
+    def rev_linkedList(self):
+
+
+if __name__ == "__main__":
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    four = Node(4)
+    list1 = LinkedList()
+
+    list1.head = one
+    one.next = two
+    two.next = three
+    three.next = four
+
+    list1.traverse()
+    print("\n\n")
