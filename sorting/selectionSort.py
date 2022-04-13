4ab304a684b0bc9ee8d31fa5615e9d4ecc1a8cb6
dynstat <vivek15@gmx.com>
2022-04-13 10:01:48 +0530

    selection sort program added

---
+++ sorting/selectionSort.py
@@ -0,0 +1,27 @@
+#  program to sort the given array using selection sort
+
+def selection_sort(arr: list):
+    length = len(arr)
+    for step in range(length):
+        min_idx = step
+        j = step + 1
+        while (j < length):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+            j += 1
+
+        # swapping arr[step] and arr[min_idx]
+        t = arr[step]
+        arr[step] = arr[min_idx]
+        arr[min_idx] = t
+
+    return arr
+
+
+# driver code
+if __name__ == "__main__":
+    unsorted_array = [4, 6, 1, 3, 9, 7, 5, 2]
+    print("Given unsorted Array: ", unsorted_array)
+    # calling the insertion sort function
+    sorted_array = selection_sort(unsorted_array)
+    print("Array after insertion sorting: ", sorted_array)
